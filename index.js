dd71445b8db796cefe443aba7fa2ee24ec443836
kiran2203 <21131a05g6@gvpce.ac.in>
2024-09-16 22:28:14 +0530

    removed_cross_env

--- index.js
+++ index.js
@@ -61,7 +61,8 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
+
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
