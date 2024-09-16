f0686b1d33b81886deacce386f197dab350b98b6
kiran2203 <21131a05g6@gvpce.ac.in>
2024-09-16 19:14:29 +0530

    Use environment variable for MongoDB connection

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://21131a05g6:<Asdfg!@1234>@cluster0.xocxm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
